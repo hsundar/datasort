ee699bb509b280147f77c2cdb0928cd4269cf21c
Karl Schulz <karl@tacc.utexas.edu>
2013-03-26 18:36:02 -0500

    add counters for xfer and sort tasks

--- sortio.h
+++ sortio.h
@@ -23,22 +23,37 @@ class sortio_Class {
   void Initialize(std::string inputfile, MPI_Comm IO_COMM);
   void Override_nFiles(int nfiles);
   void ReadFiles(); 
+  void SplitComm(MPI_Comm INCOMM);
   void Summarize();
-
 
  private:
   bool master;			   // master task?
   bool initialized;		   // class initialized?
   bool override_numfiles;          // Override num_files setting?
   bool random_read_offset;         // Randomly change rank ordering for read to minimize cache effects?
-  int  nio_tasks;		   // number of dedicated MPI I/O tasks
-  int  io_rank;			   // MPI rank of local I/O task
   int  num_files_total;		   // total # of input files to sort
   unsigned long num_records_read;  // total # of records read locally
-  MPI_Comm IO_COMM;		   // MPI communicator for raw I/O tasks
   std::string basename;		   // input file basename
   std::string indir;		   // input directory
   GRVY::GRVY_Timer_Class gt;       // performance timer
+
+  // Dedicated I/O tasks 
+
+  int      nio_tasks;		   // number of dedicated raw I/O tasks
+  int      io_rank;		   // MPI rank of local I/O task
+  MPI_Comm IO_COMM;		   // MPI communicator for raw I/O tasks
+
+  // Data transfer 
+
+  int      nxfer_tasks;		   // number of dedicated data transfer tasks
+  int      xfer_rank;		   // MPI rank of local data transfer task
+  MPI_Comm XFER_COMM;		   // MPI communicator for data transfer tasks
+
+  // Data sort 
+
+  int      nsort_tasks;		   // number of dedicated sort tasks
+  int      sort_rank;		   // MPI rank of local sort task
+  MPI_Comm SORT_COMM;		   // MPI communicator for data sort tasks
 
   unsigned char rec_buf[REC_SIZE];
   
