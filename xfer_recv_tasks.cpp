57e19b1a875b3448f94826a2cf1e41d3ec4b0939
Karl Schulz <karl@tacc.utexas.edu>
2013-04-03 07:29:45 -0500

    forgot to add new src file

---
+++ xfer_recv_tasks.cpp
@@ -0,0 +1,92 @@
+#include "sortio.h"
+
+// --------------------------------------------------------------------
+// Transfer_Tasks_Work(): work manager for data transfer tasks
+// 
+// This method configured to run on XFER_COMM
+// --------------------------------------------------------------------
+
+void sortio_Class::beginRecvTransferProcess()
+{
+  assert(initialized);
+
+  // This routine is only meaningful on a subset of XFER_COMM (the
+  // receiving tasks); the first nio_tasks in XFER_COMM are
+  // responsible for scattering the data to these receive tasks. 
+  // 
+  // See file:io_xfer_tasks.cpp for the companion sending logic.
+
+  if(!is_xfer_task)		// rules out any tasks not in XFER_COMM
+    return;
+  if(xfer_rank < nio_tasks)	// rules out the sending tasks in XFER_COMM
+    return;
+
+  int procMax;
+  int count = 0;
+
+  int recvRank = nio_tasks;
+
+  unsigned char *buffer;
+
+  buffer = (unsigned char*) calloc(MAX_FILE_SIZE_IN_MBS*1024*1024,sizeof(unsigned char));
+
+  //  while(true)
+  for(int ifile=0;ifile<num_files_total;ifile++)
+    {
+
+      if(xfer_rank == recvRank)
+	grvy_printf(INFO,"[sortio][XFER/Recv][%.4i]: file %7i of %7i\n",ifile++,num_files_total);
+#if 0
+      assert( MPI_Bcast(&procMax,1,MPI_INTEGER,0,XFER_COMM) == MPI_SUCCESS );
+
+      if(procMax == -1)		// signals that xfer process is finished
+	{
+	  grvy_printf(INFO,"[sortio][XFER/Recv][%.4i]: data XFER completed (iter=%i)\n",xfer_rank,count);
+	  break;
+	}
+      else if(procMax > 0)
+	{
+	  assert(Scatter_COMMS[procMax] != MPI_COMM_NULL);
+
+	  //const int numData = 1000000; // 100 MB file
+	  //const const int numData = 10000;
+	  //const int numData = 100000;
+	  const int numData = 18250;
+	  //const int numData = (1000000 / nscatter_tasks);
+
+	  //assert(numData < (1000000 / nxfer_tasks));
+	  //const int numData = (1000000 / nxfer_tasks);
+
+	  MPI_Scatter(NULL,numData,MPI_UNSIGNED_CHAR,
+		      &buffer[0],numData,MPI_UNSIGNED_CHAR,0,Scatter_COMMS[procMax]);
+
+	  grvy_printf(DEBUG,"[sortio][XFER/Recv][%.4i]: scatter complete from [%.4i]\n",xfer_rank,procMax);
+
+	}
+
+      if(xfer_rank == (nxfer_tasks -1))
+	grvy_printf(INFO,"[sortio][XFER/Recv][%.4i]: initiaing data XFER from [%.4i]\n",xfer_rank,procMax);
+
+#else
+      if(xfer_rank == recvRank)
+	{
+	  MPI_Status status;
+	  
+	  grvy_printf(INFO,"[sortio][XFER/Recv][%.4i]: initiating recv\n",xfer_rank);
+	  MPI_Recv(&buffer[0],100,MPI_UNSIGNED_CHAR,MPI_ANY_SOURCE,1000,XFER_COMM,&status);
+	  grvy_printf(INFO,"[sortio][XFER/Recv][%.4i]: completed recv\n",xfer_rank);
+
+	  recvRank++;
+	  if(recvRank >= nxfer_tasks)
+	    recvRank = nio_tasks;
+	}
+#endif
+
+      count++;
+    }
+
+  grvy_printf(INFO,"[sortio][XFER/Recv][%.4i]: ALL DONE\n",xfer_rank,count);
+  MPI_Barrier(XFER_COMM);
+
+  return;
+}
