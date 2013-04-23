3e37f18c69f06675a55fb903aea13ed48dc5e7eb
Karl W Schulz <karl@tacc.utexas.edu>
2013-04-23 18:01:18 -0500

    use long long, kway=32

--- include/dendro.h
+++ include/dendro.h
@@ -7,15 +7,15 @@
 #ifndef __DENDRO_H__
 #define __DENDRO_H__
 
-#ifdef __USE_64_BIT_INT__
+//#ifdef __USE_64_BIT_INT__
 #define DendroIntL long long
 #define DendroIntLSpecifier %lld
 #define DendroUIntLSpecifier %llu
-#else
-#define DendroIntL int
-#define DendroIntLSpecifier %d
-#define DendroUIntLSpecifier %u
-#endif
+//#else
+//#define DendroIntL int
+//#define DendroIntLSpecifier %d
+//#define DendroUIntLSpecifier %u
+//#endif
 
 #endif
 
