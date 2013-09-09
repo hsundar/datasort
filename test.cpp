f0a8ad017f2f2f79d8edffb016556fcfa74fbe2c
Karl W Schulz <karl@tacc.utexas.edu>
2013-09-09 07:40:12 -0500

    add additional command line argument for input file

--- test.cpp
+++ test.cpp
@@ -8,16 +8,20 @@ int main(int argc, char *argv[], char *env[])
   // [optional]: set total # of files to read via command-line;
   // otherwise, we read from input file
 
-  if(argc > 1)
-    sort_IO.overrideNumFiles(atoi(argv[1]));
-  if(argc > 2)
-    sort_IO.overrideNumIOHosts(atoi(argv[2]));
-  if(argc > 3)
-    sort_IO.overrideNumSortThreads(atoi(argv[3]));
-  if(argc > 4)
-    sort_IO.overrideNumSortGroups(atoi(argv[4]));
+  std::string input_file ("input.dat");
 
-  sort_IO.Initialize("input.dat",MPI_COMM_WORLD);
+  if(argc > 1)
+    input_file = argv[1];
+  if(argc > 2)
+    sort_IO.overrideNumFiles(atoi(argv[2]));
+  if(argc > 3)
+    sort_IO.overrideNumIOHosts(atoi(argv[3]));
+  if(argc > 4)
+    sort_IO.overrideNumSortThreads(atoi(argv[4]));
+  if(argc > 5)
+    sort_IO.overrideNumSortGroups(atoi(argv[5]));
+
+  sort_IO.Initialize(input_file,MPI_COMM_WORLD);
   sort_IO.SplitComm();
   sort_IO.Init_Read();  
   sort_IO.beginRecvTransferProcess();
